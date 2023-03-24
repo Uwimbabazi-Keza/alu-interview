b67452148715b9f8ad02ecb2892f20d5388f46bf
“Uwimbabazi-Keza” <u.keza@alustudent.com>
2023-03-24 12:01:31 -0700

    Pascals triangle

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Function that returns a list of lists of integers 
+representing the Pascal’s triangle of n
+"""
+
+
+def pascal_triangle(n):
+    """returns list of lists of integers 
+    representing the Pascal’s triangle of n
+    """
+    triangle = []
+    row = []
+    previous = []
+  
+  for i in range(0, n + 1):
+      row = [j > 0 and j < i - 1 and i > 2 and previous[j-1] + previous[j] or 1 for j in range(0, i)]
+    prev_row = row
+    triangle += [row]
+  
+  return triangle[1:]
